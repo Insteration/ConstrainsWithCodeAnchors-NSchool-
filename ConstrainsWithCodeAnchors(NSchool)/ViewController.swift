24495b34d99d0b08c535d289da99ceb3d2c0b026
Art Karma <inste@icloud.com>
2019-05-13 20:48:15 +0300

    first commit

--- ConstrainsWithCodeAnchors(NSchool)/ViewController.swift
+++ ConstrainsWithCodeAnchors(NSchool)/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let blueView: UIView = {
+        let myView = UIView()
+        myView.backgroundColor = .blue
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        return myView
+    }()
+    
+    let redView: UIView = {
+        let myView = UIView()
+        myView.backgroundColor = .red
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        return myView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.addSubview(blueView)
+        view.addSubview(redView)
+        
+        createViewRedConstraint()
+        createViewBlueConstraint()
     }
 
+    func createViewRedConstraint() {
+        redView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true //left view anchor
+        redView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
+        redView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+    }
+    
+    func createViewBlueConstraint() {
+        blueView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true //left view anchor
+        blueView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
+        blueView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        blueView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 
 }
 
